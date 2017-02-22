854c8ccecf6f7369f46d8a3ebb57a5c8a284d8a0
Bo-Xuan Fan <coffee0127@gmail.com>
2017-02-22 23:50:12 +0800

    Initial Project Commit

---
+++ front/e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, element, by } from 'protractor';
+
+export class RandomTestGeneratorPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
