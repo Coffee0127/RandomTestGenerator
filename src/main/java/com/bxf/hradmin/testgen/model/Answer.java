2ac308e84c0744ddeaa351c06d9abed90bc501ed
Bo-Xuan Fan <coffee0127@gmail.com>
2017-02-25 16:58:16 +0800

    新增問題分類、問題、問題解答 Model & Repository

---
+++ src/main/java/com/bxf/hradmin/testgen/model/Answer.java
@@ -0,0 +1,99 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Bo-Xuan Fan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.bxf.hradmin.testgen.model;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.bxf.hradmin.common.persistence.BooleanToStringConverter;
+import com.bxf.hradmin.common.persistence.IDataObject;
+import com.bxf.hradmin.common.persistence.OidGeneratorListener;
+
+/**
+ * 問題解答
+ *
+ * @since 2017-02-25
+ * @author Bo-Xuan Fan
+ */
+@Entity
+@Table(name = "answer")
+@EntityListeners(OidGeneratorListener.class)
+public class Answer implements IDataObject {
+
+    @Id
+    @Column(length = 32)
+    private String oid;
+
+    /** 答案描述 */
+    @Column(name = "description", length = 4000, nullable = false)
+    private String desc;
+
+    /** 是否為正解 */
+    @Column(name = "is_correct", columnDefinition = "CHAR", length = 1, nullable = false)
+    @Convert(converter = BooleanToStringConverter.class)
+    private Boolean isCorrect;
+
+    /** questionId */
+    @Column(name = "question_oid", length = 32, nullable = false)
+    private String questionId;
+
+    @Override
+    public String getOid() {
+        return oid;
+    }
+
+    @Override
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Boolean getIsCorrect() {
+        return isCorrect;
+    }
+
+    public void setIsCorrect(Boolean isCorrect) {
+        this.isCorrect = isCorrect;
+    }
+
+    public String getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(String questionId) {
+        this.questionId = questionId;
+    }
+
+}
---
+++ src/main/java/com/bxf/hradmin/testgen/model/Category.java
@@ -0,0 +1,89 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Bo-Xuan Fan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.bxf.hradmin.testgen.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.bxf.hradmin.common.persistence.IDataObject;
+import com.bxf.hradmin.common.persistence.OidGeneratorListener;
+
+/**
+ * 問題種類
+ * <li>Java Building Blocks
+ * <li>Operators and Statements
+ * <li>Core Java APIs
+ * <li>Methods and Encapsulation
+ * <li>Class Design
+ * <li>Exceptions
+ *
+ * @since 2017-02-24
+ * @author Bo-Xuan Fan
+ */
+@Entity
+@Table(name = "category")
+@EntityListeners(OidGeneratorListener.class)
+public class Category implements IDataObject {
+
+    @Id
+    @Column(length = 32)
+    private String oid;
+
+    /** 問題種類描述 */
+    @Column(name = "description", length = 4000, nullable = false)
+    private String desc;
+
+    /** 權重 */
+    @Column(columnDefinition = "INTEGER default 1", nullable = false)
+    private Integer weight;
+
+    @Override
+    public String getOid() {
+        return oid;
+    }
+
+    @Override
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+}
