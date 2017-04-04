3dce8dd68b6ba3b1a9fd945a0b142bf708ca41e8
Bo-Xuan Fan <coffee0127@gmail.com>
2017-04-04 10:02:40 +0800

    format server error message

--- front/src/app/shared/showMsg.ts
+++ front/src/app/shared/showMsg.ts
@@ -1,10 +1,26 @@
+import { isJsonFormat } from './utils';
+
 const $alertDialog = $('<div class="alert bg-danger" role="alert"><svg class="glyph stroked cancel"><use xlink:href="#stroked-cancel"></use></svg> <span class="errMsg"></span> <span class="pull-right btn-close-modal"><span class="glyphicon glyphicon-remove"></span></span></div>');
 
-export const showErrorMsg = function(msg: string) {
+export const showErrorMsg = (msg: string) => {
   let $errMsgModal = $('#errMsgModal');
   if ($errMsgModal.find('.errMsg').html() !== '') {
     $errMsgModal.find('.modal-dialog').append($alertDialog);
   }
-  $('.errMsg:last').html(msg && msg || 'Error Occurred...');
+  console.error(msg);
+  $('.errMsg:last').html(extractErrorMsg(msg));
   $errMsgModal.modal();
 }
+
+let extractErrorMsg = (msg) => {
+  let errMsg = 'Error Occurred...';
+  if (msg && isJsonFormat(msg)) {
+    msg = JSON.parse(msg);
+    if (msg.error && msg.message) {
+      errMsg = `[${msg.error}] ${msg.message}`;
+    } else {
+      errMsg = msg.message;
+    }
+  }
+  return errMsg;
+};
