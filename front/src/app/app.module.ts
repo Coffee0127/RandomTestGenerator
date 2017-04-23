4bf1f9c9d9fbb557c61925e5a445e5360961c395
Bo-Xuan Fan <coffee0127@gmail.com>
2017-04-23 10:55:29 +0800

    [Fixed] 程式打包未指定base href

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, RequestOptions } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -9,6 +9,8 @@ import { IndexComponent } from './index/index.component';
 import { TestPreviewComponent } from './test-generator/test-preview/test-preview.component';
 
 import { TestGeneratorService } from './test-generator/test-generator.service';
+
+import { CustomRequestOptions } from './shared/config/custom-request-options';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import { TestGeneratorService } from './test-generator/test-generator.service';
     HttpModule,
     AppRoutingModule
   ],
-  providers: [TestGeneratorService],
+  providers: [TestGeneratorService, {
+    provide: RequestOptions, useClass: CustomRequestOptions
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
